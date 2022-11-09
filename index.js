acda757ec785d9411d4a5374aad8a7520ba51d50
nasirul.islam <nasirul.islam@bracits.com>
2022-11-10 01:49:08 +0600

    add different format for download, screenshot and pdf

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ async function run() {
     await page.goto('https://bangla.bdnews24.com/');
 
     // take respected url screenshot
-    await page.screenshot({path: 'bdnews24.png'});
+    await page.screenshot({path: 'bdnews24.png', fullPage: true});
+
+    // take page screenshot in pdf 
+    await page.pdf({path: 'bdnews24.pdf', format: 'A4'});
 
     // close the browser
     await browser.close();
