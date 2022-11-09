fdb5e69c851ab4afe970f0d3ad857d17dfca6a53
nasirul.islam <nasirul.islam@bracits.com>
2022-11-10 01:34:06 +0600

    added url and run function

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const puppeteer = require('puppeteer');
 
 async function run() {
-    const browser = await puppeteer.launc();
+    const browser = await puppeteer.launch();
+
+    // init the browser
     const page = await browser.newPage();
-    await page.goto('');
+    await page.goto('https://bangla.bdnews24.com/');
 
+    // take respected url screenshot
+    await page.screenshot({path: 'bdnews24.png'});
 
+    // close the browser
     await browser.close();
 
 }
+
+run();
